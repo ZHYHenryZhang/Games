072d0e96f79457c57cbaff179044506480c9f7d1
ZHYHenryZhang <henryzhang19951213@gmail.com>
2019-03-22 23:24:00 -0700

    pymine first commit

---
+++ pymine/README.md
@@ -0,0 +1,16 @@
+# python version of game mine, just for fun
+
+## Introduction
+
+
+## File Structure
+
+
+## TODO
+ - update the class design
+ 
+
+## Update log
+
+### 03/22/19
+ - project launched
---
+++ pymine/mine.py
@@ -0,0 +1,23 @@
+""" Docstring:
+Description: Mine game in python
+Authors: 
+Date:March 22, 2019
+"""
+
+# module
+
+
+# parameters
+
+
+# objects
+
+
+# functions
+def main():
+    """ main function """
+    pass
+
+# main
+if __name__ == "__main__":
+    main()
